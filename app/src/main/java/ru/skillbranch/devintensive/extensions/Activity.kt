a7bf78a4243b38b0cfda6eefede44c4002065fb3
JohnVillip <johny2014@ukr.net>
2019-07-14 00:06:10 +0300

    hometask_3 complete 1-5 and 7 tasks

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -2,7 +2,11 @@ package ru.skillbranch.devintensive.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+
 
 fun Activity.hideKeyboard() {
     val view = this.currentFocus
@@ -14,3 +18,30 @@ fun Activity.hideKeyboard() {
 
 //    window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 }
+
+fun Activity.getRootView(): View {
+    return findViewById(android.R.id.content)
+}
+
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = getRootView().height - visibleBounds.height()
+    val marginOfError = Math.round(this.convertDpToPx(50F))
+    return heightDiff > marginOfError
+}
+
+
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
+}
+
